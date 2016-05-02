e0299254d6770964e0b77d67e11ebdcb805557d3
Anmol Ahuja <me@anmolahuja.com>
2016-05-01 22:39:08 -0700

    Inital commit.

---
+++ src/calculatebleu3.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+import os
+import sys
+
+from bleu_evaluator import BleuEvaluator
+
+DEBUG = True
+
+OUTPUT_FILE = "bleu_out.txt"
+
+def writeOutputFile(outputFilePath, val):
+    with open(outputFilePath, 'wt', encoding='utf-8') as fout:
+        fout.write('%f\n' % val)
+
+def readTokenizedLines(filePath):
+    fileTokenizedLines = []
+    with open(filePath, 'rt', encoding='utf-8') as f:
+        for line in f:
+            fileTokenizedLines.append(list(filter(lambda x: x, line.replace('\n', ' ').strip().split(' '))))
+    return fileTokenizedLines
+
+def main():
+    candidateFilePath = sys.argv[1]
+    referenceFilePath = sys.argv[2]
+    if DEBUG:
+        assert os.path.isfile(candidateFilePath)
+        assert os.path.isfile(referenceFilePath) or os.path.isdir(referenceFilePath)
+
+    candidate = readTokenizedLines(candidateFilePath)
+    references = []
+    if os.path.isfile(referenceFilePath):
+        references = [readTokenizedLines(referenceFilePath)]
+    else:
+        for reffile in os.listdir(referenceFilePath):
+            if DEBUG:
+                print("Opening reference %s" % reffile)
+            references.append(os.path.join(referenceFilePath, reffile))
+
+    if DEBUG:
+        print(candidate[0])
+        print(candidate[1])
+        print(references[0][0])
+        print(references[0][1])
+
+    beval = BleuEvaluator(references)
+    writeOutputFile(OUTPUT_FILE, beval.evaluate(candidate))
+
+if __name__ == "__main__":
+    main()
